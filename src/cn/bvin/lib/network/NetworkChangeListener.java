a59fa98a50cde06ed10f086301e167fe0f4d77a0
Bvin H. <bvin@live.cn>
2015-01-15 16:40:59 +0800

    网络检测相关

---
+++ src/cn/bvin/lib/network/NetworkChangeListener.java
@@ -0,0 +1,14 @@
+package cn.bvin.lib.network;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetworkChangeListener extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	}
+
+}
