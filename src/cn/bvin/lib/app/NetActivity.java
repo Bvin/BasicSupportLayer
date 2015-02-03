9a9aea807a351c44663d5437d1f47ee0f446ed63
Bvin H. <bvin@live.cn>
2015-02-03 13:51:08 +0800

    通知NetFragment网络状态发生变化

--- src/cn/bvin/lib/app/NetActivity.java
+++ src/cn/bvin/lib/app/NetActivity.java
@@ -1,9 +1,16 @@
 package cn.bvin.lib.app;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import cn.bvin.lib.network.NetworkHelp;
 import cn.bvin.lib.network.NetworkWatcher;
-
+/**
+ * 
+ * @ClassName: NetActivity 
+ * @Description: 通知子类和ChidFragment网络状态发生变化
+ * @author: Bvin
+ * @date: 2015年2月3日 上午11:56:53
+ */
 public class NetActivity extends WiseActivity implements NetworkWatcher{
 
 	NetworkHelp helper;
@@ -23,7 +30,7 @@ public class NetActivity extends WiseActivity implements NetworkWatcher{
 
 	@Override
 	public void change(int type) {
-		switch (type) {
+		switch (type) {//先通知NetActivity的网络状态发生变化
 		case NetworkWatcher.NETWORK_TYPE_DISCONN:
 			onNetworkDisconnect();
 			break;
@@ -43,6 +50,8 @@ public class NetActivity extends WiseActivity implements NetworkWatcher{
 		default:
 			break;
 		}
+		//再通知NetFragment网络状态发生变化
+		notifyNetFragmentNetChanged(type);
 	}
 
 	/**网络已连接*/
@@ -63,6 +72,36 @@ public class NetActivity extends WiseActivity implements NetworkWatcher{
 	/**已连接至wifi网络*/
 	protected void onWifiNetworkswitch(){
 		onNetworkConnected();
+	}
+	
+	/**通知NetFragment网络状态发生变化*/
+	private void notifyNetFragmentNetChanged(int type) {
+		if (!getSupportFragmentManager().getFragments().isEmpty()) {
+			for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+				if (fragment instanceof NetFragment) {
+					switch (type) {
+						case NetworkWatcher.NETWORK_TYPE_DISCONN:
+							((NetFragment)fragment).onNetworkConnected();
+							break;
+
+						case NetworkWatcher.NETWORK_TYPE_OTHERCONN:
+							((NetFragment)fragment).onNetworkConnected();
+							break;
+									
+						case NetworkWatcher.NETWORK_TYPE_MOBLECONN:
+							((NetFragment)fragment).onMobileNetworkSwitch();
+							break;
+							
+						case NetworkWatcher.NETWORK_TYPE_WIFICONN:
+							((NetFragment)fragment).onWifiNetworkswitch();
+							break;
+							
+						default:
+							break;
+					}
+				}
+			}
+		}
 	}
 	
 	/**是否连接上网络*/
