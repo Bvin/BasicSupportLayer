0342293f5421e077406261b48bf447596a95cd46
Bvin H. <bvin@live.cn>
2015-02-04 14:07:30 +0800

    1.添加RequestParams请求参数的构造方法 2.为RequestParams请求参数添加对应的encodeParameters()方法 2.如果没有设置请求参数就直接走GET

--- src/cn/bvin/lib/request/BvinRequest.java
+++ src/cn/bvin/lib/request/BvinRequest.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 
 
+
 import cn.bvin.lib.utils.StringUtils;
 
 import com.android.volley.AuthFailureError;
@@ -21,21 +22,10 @@ import com.android.volley.Response.ErrorListener;
  */
 public abstract class BvinRequest<T> extends Request<T>{
 
-	protected Map<String, Object> params;
 	
-	/**
-	 * 
-	 * @Title:BvinRequest
-	 * @Description:Post请求
-	 * @param url 请求地址
-	 * @param params 参数集合
-	 * @param listener 请求出错监听器
-	 */
-	public BvinRequest(String url, Map<String, Object> params, ErrorListener listener) {
-		super(Request.Method.POST, url, listener);
-		this.params = params;
-	}
-
+	protected Map<String, Object> mapParams;
+	protected RequestParams reqParams;
+	
 	/**
 	 * 
 	 * @Title:BvinRequest
@@ -47,13 +37,50 @@ public abstract class BvinRequest<T> extends Request<T>{
 		super(Request.Method.GET, url, listener);
 	}
 	
+	/**
+	 * 
+	 * @Title:BvinRequest
+	 * @Description:Post请求
+	 * @param url 请求地址
+	 * @param params 参数集合
+	 * @param listener 请求出错监听器
+	 */
+	public BvinRequest(String url, Map<String, Object> params, ErrorListener listener) {
+		super(Request.Method.POST, url, listener);
+		this.mapParams = params;
+	}
+	
+	/**
+	 * 
+	 * @Title:BvinRequest
+	 * @Description: RequestParams参数赋值的同时也会对MapParams赋值
+	 * @param url
+	 * @param params RequestParams参数集合
+	 * @param listener
+	 */
+	public BvinRequest(String url, RequestParams params, ErrorListener listener) {
+		super(Request.Method.POST, url, listener);
+		this.reqParams = params;
+		this.mapParams  = params.urlParams;
+	}
+	
+
 	@Override
 	public byte[] getBody() throws AuthFailureError {
-		if (params != null && params.size() > 0) {
-            return encodeParameters(params, getParamsEncoding());
-        } else {
+		//优先发送RequestParams形式的参数
+		if (reqParams!=null&&!reqParams.isEmpty()) {
+            return encodeParameters(reqParams, getParamsEncoding());
+        //如果 reqParams参数为空就传mapParams
+        } else if (mapParams != null && mapParams.size() > 0) {
+            return encodeParameters(mapParams, getParamsEncoding());
+        } {//如果都为空就调用父类的getBody()
         	return super.getBody();
 		}
+	}
+
+	/**对参数编码*/
+	protected byte[] encodeParameters(RequestParams reqParams, String paramsEncoding) {
+		return encodeParameters(reqParams.urlParams, paramsEncoding);
 	}
 
 	/**只拼接了参数，没有作任何加密，如需要需要，重写这个方法*/
@@ -72,8 +99,8 @@ public abstract class BvinRequest<T> extends Request<T>{
 	 * 相当于urlgetUrl()+params.toString()
 	 * */
 	public String getDebugUrl() {
-		if (params!=null&&params.size()>0) {
-			return getUrl()+"?"+StringUtils.getStringFromMap(params);
+		if (mapParams!=null&&mapParams.size()>0) {
+			return getUrl()+"?"+StringUtils.getStringFromMap(mapParams);
 		} else {
 			return getUrl();
 		}
