afb75b738d81d8dc6169a106af11cfa00123d255
Bvin H. <bvin@live.cn>
2015-02-04 14:02:00 +0800

    1.移除volley接口 2.添加getRequestActivity()方法，方便子类构造请求的时候设置请求监听 3.RequestState接口方法由RequestActivity回调

--- src/cn/bvin/lib/app/RequestFragment.java
+++ src/cn/bvin/lib/app/RequestFragment.java
@@ -3,8 +3,6 @@ package cn.bvin.lib.app;
 import cn.bvin.lib.interf.RequestListener;
 
 import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
 /**
  * 
@@ -15,23 +13,8 @@ import com.android.volley.VolleyError;
  * @date: 2015年2月3日 上午11:51:57 
  * @param <T>
  */
-public class RequestFragment<T> extends NetFragment implements RequestListener<T>,Response.ErrorListener, Listener<T>{
+public class RequestFragment<T> extends NetFragment implements RequestListener<T>{
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void onResponse(T arg0) {
-		if (getActivity() instanceof RequestActivity) {
-			((RequestActivity<T>)getActivity()).onResponse(arg0);
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public void onErrorResponse(VolleyError arg0) {
-		if (getActivity() instanceof RequestActivity) {
-			((RequestActivity<T>)getActivity()).onErrorResponse(arg0);
-		}
-	}
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -58,28 +41,23 @@ public class RequestFragment<T> extends NetFragment implements RequestListener<T
 		}
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void onRequestStart(Request<T> resquest) {
-		if (getActivity() instanceof RequestActivity) {
-			((RequestActivity<T>)getActivity()).onRequestStart(resquest);
-		}
+		
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void onRequestSuccess(T result) {
-		if (getActivity() instanceof RequestActivity) {
-			((RequestActivity<T>)getActivity()).onRequestSuccess(result);
-		}
+		
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void onRequestFailure(VolleyError error) {
-		if (getActivity() instanceof RequestActivity) {
-			((RequestActivity<T>)getActivity()).onRequestFailure(error);
-		}
+		
 	}
 	
+	@SuppressWarnings("unchecked")
+	public RequestActivity<T> getRequestActivity() {
+		return (RequestActivity<T>) getActivity();
+	}
 }
